273a36c1d882dbc07c15199521014e90ed77f847
Jan Henckens <jan@henckens.be>
2026-04-08 20:34:54 +0200

    Added full translations for nl, fr & de

---
+++ src/translations/de/translate.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'Translate' => 'Übersetzen',
+	'Translations' => 'Übersetzungen',
+	'Default' => 'Standard',
+	'Templates' => 'Templates',
+	'Modules' => 'Module',
+	'Source {language}' => 'Quelle {language}',
+	'Translation' => 'Übersetzung',
+	'Invalid Translate File path' => 'Ungültiger Dateipfad',
+	'Something went wrong while saving your translations: ' => 'Beim Speichern der Übersetzungen ist ein Fehler aufgetreten: ',
+	'Translate to' => 'Übersetzen nach',
+	'Download' => 'Herunterladen',
+	'Downloading file' => 'Datei wird heruntergeladen',
+	'Translations saved' => 'Übersetzungen gespeichert',
+	'You are editing the translations for site ' => 'Sie bearbeiten die Übersetzungen für die Website ',
+];
---
+++ src/translations/fr/translate.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'Translate' => 'Traduire',
+	'Translations' => 'Traductions',
+	'Default' => 'Par défaut',
+	'Templates' => 'Templates',
+	'Modules' => 'Modules',
+	'Source {language}' => 'Source {language}',
+	'Translation' => 'Traduction',
+	'Invalid Translate File path' => 'Chemin de fichier invalide',
+	'Something went wrong while saving your translations: ' => 'Une erreur est survenue lors de la sauvegarde de vos traductions : ',
+	'Translate to' => 'Traduire en',
+	'Download' => 'Télécharger',
+	'Downloading file' => 'Téléchargement du fichier',
+	'Translations saved' => 'Traductions sauvegardées',
+	'You are editing the translations for site ' => 'Vous modifiez les traductions pour le site ',
+];
