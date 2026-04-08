e40b984aca8a8003110252574f43ff3079f66ae7
Jan Henckens <jan@henckens.be>
2026-04-08 20:32:28 +0200

    Register the plugin as it translation source so we can translate the transation UI  #18

--- src/Translate.php
+++ src/Translate.php
@@ -10,8 +10,11 @@
 
 namespace statikbe\translate;
 
+use craft\base\Event;
 use craft\base\Plugin;
 use statikbe\translate\services\Translate as TranslateService;
+use statikbe\translate\elements\Translate as TranslateElement;
+use statikbe\translate\events\RegisterPluginTranslationEvent;
 
 /**
  * Class Translate
@@ -28,8 +31,30 @@ class Translate extends Plugin
     {
         parent::init();
 
+        Event::on(
+            TranslateElement::class,
+            TranslateElement::EVENT_REGISTER_PLUGIN_TRANSLATION,
+            function (RegisterPluginTranslationEvent $event) {
+                $event->plugins['translate'] = \Craft::$app->getPlugins()->getPlugin('translate');
+            }
+        );
+
         $this->setComponents([
             'translate' => TranslateService::class,
         ]);
+    }
+
+    public function getCpNavItem(): array
+    {
+        $label = \Craft::t('translate','Translate');
+
+        $ret = [
+            'label' => $label,
+            'url' => $this->id,
+        ];
+        if (($iconPath = $this->cpNavIconPath()) !== null) {
+            $ret['icon'] = $iconPath;
+        }
+        return $ret;
     }
 }
